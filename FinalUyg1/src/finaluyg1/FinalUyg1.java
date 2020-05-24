7d1938d674d407dc90ecb3dc92b36238359c6de6
Yasemin DEMİREL <65770236+YaseminDmrll@users.noreply.github.com>
2020-05-24 14:15:00 +0300

    Update FinalUyg1.java

--- FinalUyg1/src/finaluyg1/FinalUyg1.java
+++ FinalUyg1/src/finaluyg1/FinalUyg1.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Metotların Aşırı Yüklenmesi Yarışmacı Puanı Hesaplama Uygulaması
+Verilen değerele göre yarışmacının ismini ve puanını ekrana yazdıran  3 metotdan oluşan  bir overloading uygulamasıdır.
  */
 
 package finaluyg1;
